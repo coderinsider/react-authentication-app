41633f666669c039235ab38f150223381a710b6f
Myat Ko Ko <coderinsider1@gmail.com>
2023-09-20 01:05:57 +0630

    Adding a singup route to the server:

--- private/src/routes/signUpRoute.js
+++ private/src/routes/signUpRoute.js
@@ -1,7 +1,34 @@
+import { getDbConnection } from "../db";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 export const signUpRoute = {
     path: "/api/signup",
     method: 'post',
     handler: async (req, res) => {
+        const { email, password } = req.body;
 
+        const db  = getDbConnection('react-auth-db');
+        const user = await db.collection('users').findOne({email});
+
+        if(user) {
+            res.sendStatus(409);
+        }
+
+        const passwordHash  = await bcrypt.hash(password, 10);
+
+        const startingInfo = {
+            partnerGirl: 'Hsu',
+            hair: 'black',
+            birth: 'feb'
+        };
+
+        const results = db.collection('users').insertOne({
+            email,
+            passwordHash,
+            info: startingInfo,
+            isVerified: false
+        });
+
+        const { insertId } = results;
     }
 }
