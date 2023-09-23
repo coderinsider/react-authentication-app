611e62f95410df7b6bec0d4e5d13cebec3ecb957
Myat Ko Ko <coderinsider1@gmail.com>
2023-09-24 00:59:16 +0630

    Adding a login router to the server

--- private/src/routes/index.js
+++ private/src/routes/index.js
@@ -1,7 +1,8 @@
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
-
+import { logInRoute } from './loginRoute';
 export const routes = [
     signUpRoute,
     testRoute,
+    logInRoute
 ];
