3240a514e3a60606c3faddd491377e61025e2fcb
Myat Ko Ko <coderinsider1@gmail.com>
2023-09-25 00:19:31 +0630

    Ading JWTs to signup page

--- public/src/pages/SignUpPage.js
+++ public/src/pages/SignUpPage.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
 export const SignUpPage = () => {
+    const [ token, setToken] = useToken();
     const [errorMessage, setErrorMessage] = useState('');
 
     const [emailValue, setEmailValue] = useState('');
@@ -11,7 +13,14 @@ export const SignUpPage = () => {
     const history = useHistory();
 
     const onSignUpClicked = async () => {
-        alert('Sign up not implemented yet');
+        const response  = await axios.post('/api/signup', {
+            email: emailValue,
+            password: passwordValue
+        });
+        const { token } = response.data;
+        setToken(token);
+        history.push('/');
+        //alert('Sign up not implemented yet');
     }
 
     return (
