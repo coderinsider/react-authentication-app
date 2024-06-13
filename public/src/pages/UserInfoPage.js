2d2d564cff45a0d1cbd2934a9fdd0c3fd3d57b69
Myat Ko Ko <coderinsider1@gmail.com>
2024-06-13 21:30:23 +0630

    adding the userinterpage

--- public/src/pages/UserInfoPage.js
+++ public/src/pages/UserInfoPage.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { useToken } from '../auth/useToken';
 import { useUser } from '../auth/useUser';
-import axios from 'axios';
 
 export const UserInfoPage = () => {
     const user = useUser();
-    const [ token, setToken ] = useToken();
+    const [token, setToken] = useToken();
 
-    const { id, email, info } = user;
+    const { id, email, isVerified, info } = user;
 
     // We'll use the history to navigate the user
     // programmatically later on (we're not using it yet)
@@ -16,9 +16,9 @@ export const UserInfoPage = () => {
 
     // These states are bound to the values of the text inputs
     // on the page (see JSX below). 
-    const [ favoriteFood, setFavoriteFood ] = useState(info.favoriteFood || '');
-    const [ hairColor, setHairColor ] = useState(info.hairColor || '');
-    const [ bio, setBio ] = useState(info.bio || '');
+    const [favoriteFood, setFavoriteFood] = useState(info.favoriteFood || '');
+    const [hairColor, setHairColor] = useState(info.hairColor || '');
+    const [bio, setBio] = useState(info.bio || '');
 
     // These state variables control whether or not we show
     // the success and error message sections after making
@@ -39,36 +39,29 @@ export const UserInfoPage = () => {
     }, [showSuccessMessage, showErrorMessage]);
 
     const saveChanges = async () => {
-        // Send a request to the server to
-        // update the user's info with any changes we've
-        // made to the text input values
         try {
             const response = await axios.put(`/api/users/${id}`, {
                 favoriteFood,
                 hairColor,
-                bio
+                bio,
             }, {
-               headers: {Authorization: `Bearer ${token}`} 
+                headers: { Authorization: `Bearer ${token}` }
             });
 
             const { token: newToken } = response.data;
             setToken(newToken);
             setShowSuccessMessage(true);
-        } catch(error) {
-            setShowErrorMessage(false);
+        } catch (error) {
+            setShowErrorMessage(true);
         }
     }
 
     const logOut = () => {
-        // We'll want to log the user out here
-        // and send them to the "login page"
         localStorage.removeItem('token');
         history.push('/login');
     }
     
     const resetValues = () => {
-        // Reset the text input values to
-        // their starting values (the data we loaded from the server)
         setFavoriteFood(info.favoriteFood);
         setHairColor(info.hairColor);
         setBio(info.bio);
@@ -78,6 +71,7 @@ export const UserInfoPage = () => {
     return (
         <div className="content-container">
             <h1>Info for {email}</h1>
+            {!isVerified && <div className="fail">You won't be able to make any changes until you verify your email</div>}
             {showSuccessMessage && <div className="success">Successfully saved user data!</div>}
             {showErrorMessage && <div className="fail">Uh oh... something went wrong and we couldn't save changes</div>}
             <label>
@@ -100,8 +94,8 @@ export const UserInfoPage = () => {
             </label>
             <hr />
             <button onClick={saveChanges}>Save Changes</button>
-            <button onClick={resetValues}>Reset Record</button>
-            <button onClick={logOut}>Sign Out</button>
+            <button onClick={resetValues}>Reset Values</button>
+            <button onClick={logOut}>Log Out</button>
         </div>
     );
 }
