d2d500e0bbada06aca9dbaed6b6dfaec5c428059
Myat Ko Ko <coderinsider1@gmail.com>
2024-06-13 21:32:45 +0630

    adding the updateUserInfoRoute

--- private/src/routes/updateUserInfoRoute.js
+++ private/src/routes/updateUserInfoRoute.js
@@ -5,49 +5,48 @@ import { getDbConnection } from '../db';
 export const updateUserInfoRoute = {
     path: '/api/users/:userId',
     method: 'put',
-    handler: async (req, res, next) => {
+    handler: async (req, res) => {
         const { authorization } = req.headers;
         const { userId } = req.params;
 
-        const updateRecord = (({
+        const updates = (({
             favoriteFood,
             hairColor,
-            bio
+            bio,
         }) => ({
             favoriteFood,
             hairColor,
-            bio
+            bio,
         }))(req.body);
 
-        if(!authorization) {
-            return res.status(401).json({message: 'No authorization header sent'});
+        if (!authorization) {
+            return res.status(401).json({ message: 'No authorization header sent' });
         }
 
         const token = authorization.split(' ')[1];
 
         jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-            if(err) return res.status(401).json({message: 'Unable to access'});
+            if (err) return res.status(401).json({ message: 'Unable to verify token' });
 
-            const { id } = decoded;
+            const { id, isVerified } = decoded;
 
-            if(id !== userId) return res.status(403).json({message: 'Not allowed to updated that user'});
+            if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
+            if (!isVerified) return res.status(403).json({ message: 'You need to verify your email before you can update your data'})
+
             const db = getDbConnection('react-auth-db');
-            const results = await db.collection('users').findOneAndUpdate(
-                {_id: ObjectID(id)},
-                {$set: {info: updateRecord}},
-                {returnOriginal: false}
+            const result = await db.collection('users').findOneAndUpdate(
+                { _id: ObjectID(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false },
             );
-            const { email, isVerified, info } = results.value;
-            jwt.sign({id, email, isVerified, info}, process.env.JWT_SECRET, { expiresIn: '2d'}, (err, token) => {
-                if(err) {
+            const { email, info } = result.value;
+
+            jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
                     return res.status(200).json(err);
                 }
-                res.status(200).json({token});
+                res.status(200).json({ token });
             });
-
-
-
-
-        });
+        })
     }
 }
