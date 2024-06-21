70ecb3ebfce6df115c98e52986480025a82c247b
Myat Ko Ko <coderinsider1@gmail.com>
2024-06-22 00:03:51 +0630

    Sending a rest password email

--- private/src/routes/index.js
+++ private/src/routes/index.js
@@ -3,11 +3,12 @@ import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmailRoute';
-
+import { forgotPasswordRoute } from './forgotPasswordRoute';
 export const routes = [
     logInRoute,
     signUpRoute,
     testRoute,
     updateUserInfoRoute,
     verifyEmailRoute,
+    forgotPasswordRoute
 ];
